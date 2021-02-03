f665fdc932bfcb1e577b66c9069e3c1389728f20
Bitsy-Chuck <singhtejasv9@gmail.com>
2021-02-03 23:53:23 +0530

    Add APU

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Common from './Common';
 import web from "../src/images/gullak.jpg";
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+	const [testVar, setTestVar] = useState("ping");
+	useEffect(() => {
+		fetch('/ping').then(res => res.json()).then(data => {
+		  setTestVar(data.reply);
+		});
+	  }, []);
 	return (
 		<>
-			<Common  name='Grow your business with' imgsrc={web} visit='/services' btname='Get Started'/>	
+			<Common  name='Grow your business with' imgsrc={web} visit='/services' btname='Get Started'/>
+			<h>
+				Test API {testVar}
+			</h>
 		</>
 	);
 };
