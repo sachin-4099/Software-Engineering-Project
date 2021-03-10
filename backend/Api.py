bb5f77cbb54acc959eff7daa6c11c72f2855082d
Bitsy-Chuck <singhtejasv9@gmail.com>
2021-03-11 01:30:00 +0530

    Add getCategory API

--- backend/Api.py
+++ backend/Api.py
@@ -1,6 +1,6 @@
-from flask import Flask,request, jsonify, Response
+from flask import Flask,request, jsonify, Response, json
 import DB as db
-
+import FrontendService as FrontendService
 
 app = Flask(__name__)
 
@@ -27,9 +27,36 @@ def authUser():
     password=req.get('password')
     val = db.auth_user(uname, password)
     if(val[0]==False):
-        return Response("{'success': 'False'}", status=401, mimetype='application/json')
+        return Response("{'success': False}", status=401, mimetype='application/json')
     else:
-        return Response("{'success': 'True'}", status=200, mimetype='application/json')
+        return Response(jsonify("{'success': 'True'}"), status=200, mimetype='application/json')
+
+@app.route("/confirm_payement_merchant", methods=["POST"])
+def confirmPayment():
+    req = request.json
+    print(req)
+    username= req.get("username")
+    amount = req.get("amount")
+    currency = req.get("currency")
+    payment_category_id= req.get("payment_category_id")
+    percentage_category = req.get("percentage_category")
+    coupon_id = req.get("coupon_id")
+    merchant_id = req.get("merchant_id")
+    PaymentService.checkCouponCodeValidity(coupon_code)
+    PaymentService.generateOrderId(username, amount, currency, payment_category, percentage_category,  merchant_id)
+
+
+@app.route("/list/category", methods=["GET"])
+def getCategory():
+    userid = request.args.get('userid')
+    _response = FrontendService.getCategory(userid)
+    response = app.response_class(
+        response=json.dumps(_response),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ backend/PaymentService.py
@@ -0,0 +1,23 @@
+import razorpay
+import configparser
+import pprint
+import json
+
+config= configparser.ConfigParser()
+config.read('creds.ini')
+USER_KEY = config['RAZORPAY']['key_id']
+SECRET_KEY = config['RAZORPAY']['key_secret']
+client = razorpay.Client(auth=(USER_KEY, SECRET_KEY))
+
+order_amount = 50000
+order_currency = 'INR'
+order_receipt = 'order_rcptid_11'
+data = {
+        "amount" : 5000,
+        "currency": "INR",
+        "receipt": "order_rcptid_11"
+    }
+resp = client.order.create(data= data)
+print(type(resp))
+tt= json.dumps(resp, indent=4)
+print(tt)
