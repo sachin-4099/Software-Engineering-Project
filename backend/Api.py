782199215659a34e4f27d96cb23fc32bab2c10a4
Bitsy-Chuck <singhtejasv9@gmail.com>
2021-03-11 02:25:52 +0530

    Add Get Merchant API

--- backend/Api.py
+++ backend/Api.py
@@ -57,6 +57,15 @@ def getCategory():
     )
     return response
 
+@app.route("/list/merchant", methods=["GET"])
+def getMerchantList():
+    _response = FrontendService.getMerchantList()
+    response = app.response_class(
+        response=json.dumps(_response),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__=="__main__":
     app.run(debug=True)
