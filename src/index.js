5a21ff7323806ff848160a482dc187c9e15f4386
Sachin Agarwal <sachinagarwal0499@gmail.com>
2021-04-22 11:37:22 +0530

    Deployment Changes

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+if (process.env.NODE_ENV === "production"{
+      app.use(express.static("build"));
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      });
+    }
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
